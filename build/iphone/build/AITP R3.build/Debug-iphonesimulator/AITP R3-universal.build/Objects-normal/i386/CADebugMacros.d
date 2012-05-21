09d16d11fffe1b2cea45ffab2a6a13e0a8f0c95c
Brady Higginbotham <bradyhigginbotham07@gmail.com>
2012-05-21 17:55:43 -0500

    new stuff

---
+++ build/iphone/build/AITP R3.build/Debug-iphonesimulator/AITP R3-universal.build/Objects-normal/i386/CADebugMacros.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/Stick/Documents/Titanium\ Studio\ Workspace/R3-App/build/iphone/Classes/CADebugMacros.cpp \
+  /Users/Stick/Documents/Titanium\ Studio\ Workspace/R3-App/build/iphone/Classes/CADebugMacros.h
