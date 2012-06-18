c9cfab7089f54abb25e44f4dcbe8ea70901c4305
Brady Higginbotham <bradyhigginbotham07@gmail.com>
2012-06-18 13:57:50 -0500

    splash pages

---
+++ build/iphone/build/AITP R3.build/Debug-iphonesimulator/AITP R3-universal.build/Objects-normal/i386/NSObject+SBJSON.d
@@ -0,0 +1,5 @@
+dependencies: \
+  /Users/Stick/Documents/Titanium\ Studio\ Workspace/AITP\ R3/build/iphone/Classes/JSON/NSObject+SBJSON.m \
+  /Users/Stick/Documents/Titanium\ Studio\ Workspace/AITP\ R3/build/iphone/Classes/JSON/NSObject+SBJSON.h \
+  /Users/Stick/Documents/Titanium\ Studio\ Workspace/AITP\ R3/build/iphone/Classes/JSON/SBJsonWriter.h \
+  /Users/Stick/Documents/Titanium\ Studio\ Workspace/AITP\ R3/build/iphone/Classes/JSON/SBJsonBase.h
