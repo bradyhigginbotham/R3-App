efe8d9a6762ca861b2f3f0503dda0f311597f60d
unknown <Dustin@Dustin-HP.(none)>
2012-06-19 13:58:32 -0500

    Minor changes

---
+++ Resources/ui/common/certifications/DetailView.js
@@ -0,0 +1,29 @@
+functin DetailView()
+{
+	var self = Ti.UI.createWindow
+	({
+		backgroundColor: white
+	});
+	
+	var lbl = Ti.UI.createLabel
+	({
+		text: 'Certification Details',
+		height: 'auto',
+		width: 'auto',
+		color: '#000'		
+	});
+	
+	self.add(lbl);
+	
+	self.addEventListener('itemSelected',function(e)
+	{
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+}
