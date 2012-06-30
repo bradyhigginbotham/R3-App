d3e77b7534cc1580ba2d67ef3b0c889538c19d94
Brady Higginbotham <bradyhigginbotham07@gmail.com>
2012-06-30 13:59:15 -0500

    detail view

--- Resources/ui/common/competitions/ListView.js
+++ Resources/ui/common/competitions/ListView.js
@@ -10,30 +10,14 @@ function ListView() {
     //Get competitions from database
     var resultSet = db.execute('SELECT * FROM competitions ORDER BY date ASC');
     while (resultSet.isValidRow()) {
-    	if(header != resultSet.fieldByName('date')) { // new state
-    		header = resultSet.fieldByName('date')
-			results.push({
-			    id: resultSet.fieldByName('id'),
-				competition: resultSet.fieldByName('competition'),
-				day: resultSet.fieldByName('day'),
-				date: resultSet.fieldByName('date'),
-				description: resultSet.fieldByName('description'),
-				schedule_id: resultSet.fieldByName('schedule_id'),
-				hasChild: true,
-				header: header,
-				height: 40
-			});
-		} else{
-			results.push({
-                id: resultSet.fieldByName('id'),
-				competition: resultSet.fieldByName('competition'),
-				day: resultSet.fieldByName('day'),
-				date: resultSet.fieldByName('date'),
-				description: resultSet.fieldByName('description'),
-				hasChild: true,
-				height: 40
-			});			
-		}
+		results.push({
+            id: resultSet.fieldByName('id'),
+			competition: resultSet.fieldByName('competition'),
+			description: resultSet.fieldByName('description'),
+			schedule_id: resultSet.fieldByName('schedule_id'),
+			hasChild: true,
+			height: 40
+		});			
     	resultSet.next();
     }
     resultSet.close();
