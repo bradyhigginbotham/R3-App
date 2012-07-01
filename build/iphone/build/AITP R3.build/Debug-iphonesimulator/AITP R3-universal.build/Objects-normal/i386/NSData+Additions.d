fd3995dae3df8c32a638d9d41383d48f4f450b3a
unknown <Dustin@Dustin-HP.(none)>
2012-06-30 22:10:16 -0500

    Update

--- build/iphone/build/AITP R3.build/Debug-iphonesimulator/AITP R3-universal.build/Objects-normal/i386/NSData+Additions.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/Stick/Documents/Titanium\ Studio\ Workspace/AITP\ R3/build/iphone/Classes/NSData+Additions.mm \
-  /Users/Stick/Documents/Titanium\ Studio\ Workspace/AITP\ R3/build/iphone/Classes/NSData+Additions.h \
-  /Users/Stick/Documents/Titanium\ Studio\ Workspace/AITP\ R3/build/iphone/Classes/Base64Transcoder.h
