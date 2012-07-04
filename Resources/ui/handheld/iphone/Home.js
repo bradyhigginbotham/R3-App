65c28f97a7ed4397e0212eab5650be734608dd1c
Brady Higginbotham <bradyhigginbotham07@gmail.com>
2012-07-03 21:25:43 -0500

    images button

--- Resources/ui/handheld/iphone/Home.js
+++ Resources/ui/handheld/iphone/Home.js
@@ -53,20 +53,38 @@ function HomeWindow() {
 	});
 	announcements.appendRow(announcementRow);
 
-	/*---- Home Icons ----*/
+	/*---- Icon Views ----*/
 	var eventIcons = Ti.UI.createView({
 		backgroundImage: 'NONE',
 		top: 0,
 		height: 150,
 		layout: 'horizontal'
-	})
+	});
+	
+	var resourceIcons = Ti.UI.createView({
+		backgroundImage: 'NONE',
+		top: 0,
+		height: 150,
+		layout: 'horizontal'
+	});	
+	
+	var imageUploadButton = Ti.UI.createButton({
+		title: "Images",
+		height: iconHeight,
+		width: iconWidth,
+		top: iconTop,
+		left: iconLeft
+	});
+	resourceIcons.add(imageUploadButton);
+
+	/*---- Home Icons ----*/
 	var collegesButton = Ti.UI.createButton({
 		title: "Colleges",
 		height: iconHeight,
 		width: iconWidth,
 		top: iconTop,
 		left: iconLeft
-	})
+	});
 	eventIcons.add(collegesButton);
 	
 	var schedulesButton = Ti.UI.createButton({
@@ -75,7 +93,7 @@ function HomeWindow() {
 		width: iconWidth,
 		top: iconTop,
 		left: iconLeft
-	})
+	});
 	eventIcons.add(schedulesButton);
 	
 	var mapsButton = Ti.UI.createButton({
@@ -84,7 +102,7 @@ function HomeWindow() {
 		width: iconWidth,
 		top: iconTop,
 		left: iconLeft
-	})
+	});
 	eventIcons.add(mapsButton);
 	
 	var presentationsButton = Ti.UI.createButton({
@@ -93,7 +111,7 @@ function HomeWindow() {
 		width: iconWidth,
 		top: iconTop,
 		left: iconLeft
-	})
+	});
 	eventIcons.add(presentationsButton);
 	
 	var competitionsButton = Ti.UI.createButton({
@@ -102,7 +120,7 @@ function HomeWindow() {
 		width: iconWidth,
 		top: iconTop,
 		left: iconLeft
-	})
+	});
 	eventIcons.add(competitionsButton);
 	
 	var certificationsButton = Ti.UI.createButton({
@@ -111,7 +129,7 @@ function HomeWindow() {
 		width: iconWidth,
 		top: iconTop,
 		left: iconLeft
-	})
+	});
 	eventIcons.add(certificationsButton);
 	
 	/*---- Icon EventListeners ----*/
@@ -173,10 +191,6 @@ function HomeWindow() {
 		width: tabWidth
 	});
 	mainNavWindow.add(resourcesTab);
-	
-	var resourceIcons = Ti.UI.createView({ backgroundImage:'NONE' });
-	var label = Ti.UI.createLabel({text: 'Resources'});
-	resourceIcons.add(label);
 	
 	var scrollableView = Ti.UI.createScrollableView({
 	  views:[eventIcons,resourceIcons],
