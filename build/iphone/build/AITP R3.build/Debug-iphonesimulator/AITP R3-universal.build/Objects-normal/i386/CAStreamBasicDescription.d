671132a5d343637802f8950a573f040d4c867aad
Brady Higginbotham <bradyhigginbotham07@gmail.com>
2012-10-09 00:18:23 -0500

    deleted old files

--- build/iphone/build/AITP R3.build/Debug-iphonesimulator/AITP R3-universal.build/Objects-normal/i386/CAStreamBasicDescription.d
+++
@@ -1,5 +0,0 @@
-dependencies: \
-  /Users/Stick/Documents/Titanium\ Studio\ Workspace/AITP\ R3/build/iphone/Classes/CAStreamBasicDescription.cpp \
-  /Users/Stick/Documents/Titanium\ Studio\ Workspace/AITP\ R3/build/iphone/Classes/CAStreamBasicDescription.h \
-  /Users/Stick/Documents/Titanium\ Studio\ Workspace/AITP\ R3/build/iphone/Classes/CADebugMacros.h \
-  /Users/Stick/Documents/Titanium\ Studio\ Workspace/AITP\ R3/build/iphone/Classes/CAMath.h
--- build/iphone/build/AITP R3.build/Debug-iphonesimulator/AITP R3-universal.build/Script-DABB369F12E8CB280026A6EA.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-"/Users/Stick/Library/Application Support/Titanium/mobilesdk/osx/2.1.2.GA/iphone/builder.py" xcode
-exit $?
